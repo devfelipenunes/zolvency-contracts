80c6dd2d3ea551369f18cdb49842cc8fcf32d216
Felipe Nunes Oliveira <fnunes@mcity388.aquarius.cpqd.com.br>
2026-04-17 18:21:26 -0300

    test(evm): add axelar verifier deploy script

---
+++ packages/evm/script/DeployAxelarVerifier.s.sol
@@ -0,0 +1,18 @@
+// packages/evm/script/DeployAxelarVerifier.s.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "forge-std/Script.sol";
+import "../src/ZolvencyVerifierAxelar.sol";
+
+contract DeployAxelarVerifier is Script {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("EVM_PRIVATE_KEY");
+        address gateway = 0xe432150cce91c13a887f7D836923d5597adD8E31; // Axelar Sepolia Gateway
+        string memory stellarSource = vm.envString("STELLAR_IDENTITY_ADDRESS");
+
+        vm.startBroadcast(deployerPrivateKey);
+        ZolvencyVerifierAxelar verifier = new ZolvencyVerifierAxelar(gateway, stellarSource);
+        console.log("ZolvencyVerifierAxelar deployed to:", address(verifier));
+        vm.stopBroadcast();
+    }
+}
